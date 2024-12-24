000e4311a9af098bc45191b76ffbaefca74faf23
tejahack <155117019+tejahack@users.noreply.github.com>
2024-12-24 10:37:00 +0530

    Add files via upload

---
+++ build/static/js/main.70b68089.js
@@ -0,0 +1,2 @@
+document.getElementById("clickMe").addEventListener("click",(function(){document.getElementById("message").innerText="Hello, you clicked the button!"}));
+//# sourceMappingURL=main.70b68089.js.map
