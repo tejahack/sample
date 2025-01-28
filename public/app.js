fce4b4ce34a9bbd52a0db7a83b7d3105e9d1986a
tejahack <155117019+tejahack@users.noreply.github.com>
2025-01-28 06:32:43 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,43 +1,11 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Nodemailer configuration
-const transporter = nodemailer.createTransport({
-    service: 'gmail', // Use your email service
-    auth: {
-        user: 'molletiteja143@gmail.com', // Your email
-        pass: 'mteja1234' // Your email password or app password
-    }
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
 });
 
-// Endpoint to send email
-app.post('/send-email', (req, res) => {
-    const { name, email, message } = req.body;
-
-    const mailOptions = {
-        from: email,
-        to: 'molletiteja4@gmail.com', // Updated admin email to receive messages
-        subject: `Contact Form Submission from ${name}`,
-        text: message
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return res.status(500).send(error.toString());
-        }
-        res.status(200).send('Email sent: ' + info.response);
-    });
-});
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
 });
